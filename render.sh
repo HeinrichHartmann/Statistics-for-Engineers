fd2114a4ae7a3724ce58037aae2111a28ddae036
Heinrich Hartmann <hartmann@uni-koblenz.de>
2015-05-12 18:29:01 +0200

    Presentation fixes.

--- render.sh
+++ render.sh
@@ -5,6 +5,6 @@ PID=`cat slides.pid`
 echo "Killing process $PID"
 kill $PID
 
-~/Programs/anaconda/bin/ipython nbconvert Tutorial.ipynb --to slides --post serve &
+~/Programs/anaconda/bin/ipython nbconvert "$1" --to slides --post serve &
 
 echo $! > slides.pid
